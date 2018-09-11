72332555068f9b5df13c4af9c0eabb3271714467
Stephen Chu <stephen.chu530@gmail.com>
2018-09-11 11:23:17 -0700

    refactor: removed excess else statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
+        offset = 0
         if ord(c) > 96 and ord(c) < 123:
             offset = -32
-        else:
-            offset = 0
         print("{:s}".format(chr(ord(c) + offset)), end='')
     else:
         print()
