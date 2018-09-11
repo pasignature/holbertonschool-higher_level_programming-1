feeda2288e7b5d2c2a737304e4a413c46dd3609a
Stephen Chu <stephen.chu530@gmail.com>
2018-09-11 12:26:09 -0700

    fix: changed logic for handlng ord to chr

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if c is not ord('e') or c is not ord('q'):
+for c in range(97, 123):
+    if c is not 101 or c is not 113:
         print("{:s}".format(chr(c)), end='')
