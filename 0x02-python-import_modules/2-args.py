928566a9c9f2a890ecced0fe06e716ffb21f002f
Stephen Chu <stephen.chu530@gmail.com>
2018-09-13 12:28:33 -0700

    fix: changed how to iterate

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    count = len(argv) - 1
-    if not count:
+    count = len(argv)
+    if count == 1:
         print("0 arguments.")
-    elif count == 1:
+    elif count == 2:
         print("1 argument:")
         print("1: {:s}".format(argv[1]))
     else:
-        print("{:d}: arguments:".format(count))
-        for n,i in enumerate(argv[1:]):
-            print("{:d}: {:s}".format(n + 1, i))
+        print("{:d}: arguments:".format(count - 1))
+        num = 1
+        for i in range(1, count):
+            print("{:d}: {:s}".format(num, argv[i]))
+            num += 1;
