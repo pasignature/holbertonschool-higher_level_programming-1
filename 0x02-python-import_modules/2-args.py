9ef8b642a266c50cdbf60b96c7c06339a9c75a84
Stephen Chu <stephen.chu530@gmail.com>
2018-09-13 12:36:25 -0700

    fix: fixed typos - had extra chars

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {:s}".format(argv[1]))
     else:
-        print("{:d}: arguments:".format(count - 1))
+        print("{:d} arguments:".format(count - 1))
         num = 1
         for i in range(1, count):
             print("{:d}: {:s}".format(num, argv[i]))
