1cefe9d2f548cf09660eadbebe1ade6f65b1fb0f
Stephen Chu <stephen.chu530@gmail.com>
2018-09-18 12:54:30 -0700

    fix: saved changes to original list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,4 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return my_list[0:idx] + my_list[idx + 1:]
+    my_list = my_list[0:idx] + my_list[idx + 1:]
+    return my_list
