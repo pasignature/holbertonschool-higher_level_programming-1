a10c11fd9c3fb670dbb1fed6ee59ea284699bdad
Stephen Chu <stephen.chu530@gmail.com>
2018-09-18 19:28:34 -0700

    fix: changed to brute force sumnation soln

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the singly linked list
@@ -6,27 +7,22 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *frnt = *head, *bck = *head, *end = NULL;
+	listint_t *walk = *head;
+	int i = 0, count = 0, mid = 0;
+	ssize_t sum1 = 0, sum2 = 0;
 
 	if (!head)
 		return (0);
 	if (!*head || !(*head)->next)
 		return (1);
-	bck = frnt->next;
-	while (bck->next)
-		bck = bck->next;
-	while (frnt->n == bck->n)
-	{
-		end = bck;
-		if (frnt == bck || frnt->next == bck || frnt->next->next == bck)
-			return (1);
-		else
-		{
-			frnt = frnt->next;
-			bck = frnt->next;
-			while (bck->next != end)
-				bck = bck->next;
-		}
-	}
+	for (; walk; count++, walk = walk->next)
+		sum1 += walk->n;
+	mid = count / 2;
+	for (walk = *head, i = 0; i < mid; walk = walk->next, i++)
+		sum2 += walk->n;
+	if (count % 2)
+		sum1 -= walk->n;
+	if (sum1 / 2 == sum2)
+		return (1);
 	return (0);
 }
