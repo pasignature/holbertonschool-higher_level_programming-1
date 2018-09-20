e9ecffe2989a2d2e9356745833035e1dc345f83f
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 12:14:19 -0700

    fix: changed return value for incorrect input checks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 def roman_to_int(roman_string):
     r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) is not str:
-        return None
+        return 0
     for c in roman_string:
         if c not in ['I', 'V', 'X', 'L', 'C', 'D', 'M']:
-            return None
+            return 0
     total = 0
     prev = 0
     for c in roman_string:
