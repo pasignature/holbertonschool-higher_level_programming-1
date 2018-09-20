89c78ffb7ef6ab36a04f6d898887606df2e2ec81
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 11:54:42 -0700

    feat: added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    if roman_string is None or type(roman_string) is not str:
+        return None
+    for c in roman_string:
+        if c not in ['I', 'V', 'X', 'L', 'C', 'D']:
+            return None
+    total = 0
+    prev = 0
+    for c in roman_string:
+        total += r_n[c]
+        if r_n[c] > prev:
+            total -= 2 * prev
+        prev = r_n[c]
+    return total
