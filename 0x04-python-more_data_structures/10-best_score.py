29a84a213dc6b5315815d7537612beda1e1a591a
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 12:04:10 -0700

    style: fixed incorrect pep8 style guide for None cond checks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     name, max = None, None
-    if a_dictionary is not None:
-        for k, v in a_dictionary.items():
-            if name == None:
-                name, max = k, v
-            elif v > max:
-                name = k
-                max = v
+    for k, v in a_dictionary.items():
+        if name == None:
+            name, max = k, v
+        elif v > max:
+            name = k
+            max = v
     return name
