b0f346e0d77964c0459e0402eb10ad64c1fe72ad
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 12:10:44 -0700

    fix: added missing 'M' Roman Numeral to dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) is not str:
         return None
     for c in roman_string:
