44cbe412c00c96f6f5b1d24ba87221d1192b8c0c
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 11:26:49 -0700

    feat: added 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    name, max = None, None
+    if a_dictionary is not None:
+        for k, v in a_dictionary.items():
+            if name == None:
+                name, max = k, v
+            elif v > max:
+                name = k
+                max = v
+    return name
