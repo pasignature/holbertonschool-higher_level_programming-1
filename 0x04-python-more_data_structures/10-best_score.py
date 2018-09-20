87903420980e0f553ba3ac64d2cb05d976050023
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 12:05:56 -0700

    style: fixed the actual None cond check error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
     name, max = None, None
-    for k, v in a_dictionary.items():
-        if name == None:
-            name, max = k, v
-        elif v > max:
-            name = k
-            max = v
+    if a_dictionary is not None:
+        for k, v in a_dictionary.items():
+            if name is None:
+                name, max = k, v
+            elif v > max:
+                name = k
+                max = v
     return name
