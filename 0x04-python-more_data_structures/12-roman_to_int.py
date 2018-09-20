c863fee2d27b13742e0bc581b778a88bcadab04b
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 16:56:06 -0700

    refactor: removed char check because it's not necessary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,6 @@ def roman_to_int(roman_string):
     r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) is not str:
         return 0
-    for c in roman_string:
-        if c not in r_n.keys():
-            return 0
     total = 0
     prev = 0
     for c in roman_string:
