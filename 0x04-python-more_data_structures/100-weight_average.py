c16f3078a90bc4385f67c448434ae4435ddb3d05
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 15:54:44 -0700

    fix: added empty list check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not len(my_list):
+        return 0
     num = sum(map(lambda x: x[0] * x[1], my_list))
     den = sum(map(lambda x: x[1], my_list))
     return num / den
