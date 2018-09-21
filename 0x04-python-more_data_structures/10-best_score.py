687941a94d18d9cf7894eb91e7c43f5fb6f1fe9c
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 17:57:11 -0700

    refactor: removed some redundancies

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     name, max = None, None
-    if a_dictionary is not None:
+    if a_dictionary:
         for k, v in a_dictionary.items():
-            if name is None:
+            if not max or v > max:
                 name, max = k, v
-            elif v > max:
-                name = k
-                max = v
     return name
