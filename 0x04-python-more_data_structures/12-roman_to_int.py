744612d001d43cfc4b862dca762c38761ddc8eab
Stephen Chu <stephen.chu530@gmail.com>
2018-09-20 17:03:53 -0700

    refactor: attemping pythonic way

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,8 @@ def roman_to_int(roman_string):
     r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) is not str:
         return 0
-    total = 0
-    prev = 0
+    total, prev = 0, 0
     for c in roman_string:
-        total += r_n[c]
-        if r_n[c] > prev:
-            total -= 2 * prev
+        total += r_n[c] - 2 * prev if r_n[c] > prev else r_n[c]
         prev = r_n[c]
     return total
