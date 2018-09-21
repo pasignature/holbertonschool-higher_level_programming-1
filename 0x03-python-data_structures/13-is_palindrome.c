08964c5c15f9ca7f653aac0439f3048ad21b4af3
Stephen Chu <stephen.chu530@gmail.com>
2018-09-21 06:58:03 -0700

    fix: attempted recursive soln

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,22 +7,36 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *walk = *head;
-	int i = 0, count = 0, mid = 0;
-	ssize_t sum1 = 0, sum2 = 0;
+	listint_t **frnt = head;
 
 	if (!head)
 		return (0);
 	if (!*head || !(*head)->next)
 		return (1);
-	for (; walk; count++, walk = walk->next)
-		sum1 += walk->n;
-	mid = count / 2;
-	for (walk = *head, i = 0; i < mid; walk = walk->next, i++)
-		sum2 += walk->n;
-	if (count % 2)
-		sum1 -= walk->n;
-	if (sum1 / 2 == sum2)
-		return (1);
-	return (0);
+	return (ip_helper(frnt, (*head)->next));
+}
+
+#include <stdio.h>
+/**
+ * ip_helper - recursive function that does the actual checking if is palindrome
+ * @frnt: pointer to front of linked list
+ * @bck: pointer to back of linked list
+ * Return: 1 if match, 0 if not
+ */
+int ip_helper(listint_t **frnt, listint_t *bck)
+{
+	if (bck->next)
+	{
+		if (!ip_helper(frnt, bck->next))
+			return (0);
+	}
+	else
+	{
+		if ((*frnt)->n != bck->n)
+			return (0);
+		if (*frnt == bck || (*frnt)->next == bck)
+			return (1);
+		*frnt = (*frnt)->next;
+	}
+	return (1);
 }
