0f73337d9f4b89508adb91d21c301bc1db25c0ab
Stephen Chu <stephen.chu530@gmail.com>
2018-10-01 12:11:09 -0700

    fix: changed order of exception catch

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,15 +2,17 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
-        ans = 0
         try:
             ans = int(my_list_1[i]) / int(my_list_2[i])
-        except ZeroDivisionError:
-            print("division by 0")
-        except ValueError:
-            print("wrong type")
         except IndexError:
             print("out of range")
+            ans = 0
+        except ValueError:
+            print("wrong type")
+            ans = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            ans = 0
         finally:
             result.append(ans)
     return result
