837db5b1d0a79b73ca911a7f83024006d4b8ca97
Stephen Chu <stephen.chu530@gmail.com>
2018-10-02 11:23:25 -0700

    fix: renamed size var to value

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -12,14 +12,14 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """Size Setter"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
-        elif size < 0:
+        elif value < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """Calculates the area"""
