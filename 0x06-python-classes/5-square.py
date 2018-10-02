a6f1857f442448037968cf8c12e046e543e1b661
Stephen Chu <stephen.chu530@gmail.com>
2018-10-02 11:15:26 -0700

    feat: intial commit for 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+class Square:
+    """Basic square class #5"""
+
+    def __init__(self, size=0):
+        """Initialize the square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Size Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Size Setter"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints to stdout the square using # symbol"""
+        row = ''.join('#' for c in range(self.size))
+        print('\n'.join(row for r in range(self.size)))
