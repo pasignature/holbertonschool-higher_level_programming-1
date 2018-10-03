5ad079f840596939ea76260f67997ebfb15cbf39
Stephen Chu <stephen.chu530@gmail.com>
2018-10-02 19:30:21 -0700

    fix: removed else clause

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 class MagicClass:
-    """magic class"""
+    """magic class
+    filler text 1
+    filler text 2
+    """
 
     def __init__(self, radius):
         """Initialize magic class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError('radius must be a number')
-        else:
-            self.__radius = radius
+            raise TypeError('radius must be a number')
+        self.__radius = radius
 
     def area(self):
         """Calculate area"""
