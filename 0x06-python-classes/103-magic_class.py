8a9d4f359ae0a875e1ce5d023ba25db8df723ec1
Stephen Chu <stephen.chu530@gmail.com>
2018-10-02 18:52:31 -0700

    feat: initial commit for 103-magic_class.py

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,14 @@
+#!/use/bin/python3
+class MagicClass:
+    def __init__(self, radius):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must bye a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
