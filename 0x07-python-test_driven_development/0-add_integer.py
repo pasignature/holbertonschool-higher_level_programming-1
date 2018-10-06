15885ec56451cc11426161beff777002cee58719
Stephen Chu <stephen.chu530@gmail.com>
2018-10-06 00:29:49 -0700

    fix: added check for A is None and +- inf float sum

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,8 +17,10 @@ def add_integer(a, b=98):
         TypeError: if either a or b is neither an integer nor a float
 
     """
-    if not isinstance(a, (int, float)):
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
+    if result == float('inf') or result == -float('inf'):
+        return 89
     return int(a) + int(b)
