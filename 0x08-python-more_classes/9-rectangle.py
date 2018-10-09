c245fa1dcc9cdc9a086495e07931ccf2a842a42c
Stephen Chu <stephen.chu530@gmail.com>
2018-10-09 06:21:11 -0700

    fix: corrected wrong indentation

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -60,14 +60,14 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-       if not isinstance(rect_1, Rectangle):
-           raise TypeError("rect_1 must be an instance of Retangle")
-       if not isinstance(rect_2, Rectangle):
-           raise TypeError("rect_2 must be an instance of Retangle")
-       if rect_1.area() >= rect_2.area():
-           return rect_1
-       if rect_1.area() < rect_2.area():
-           return rect_2
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Retangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Retangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        if rect_1.area() < rect_2.area():
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
