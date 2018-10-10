10d5c6c0c4fd4b9eb3c8b8be3e4e0b099da7e2f5
Stephen Chu <stephen.chu530@gmail.com>
2018-10-09 22:08:52 -0700

    refactor: finally figured out the weird way to do this

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    j = __import__("__main__").i
-    return ", ".join(["Holberton"] * j)
+def magic_string(lst=[]):
+    lst.append("Holberton")
+    return ", ".join(lst)
