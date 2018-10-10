c18220efc3ce8e8b13fb51cfd9836e420022f539
Stephen Chu <stephen.chu530@gmail.com>
2018-10-09 18:03:41 -0700

    fix: changed to using __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name is 'first_name':
-            self.__dict__[name] = value
-        else:
-            cls = type(self).__name__
-            errmsg = "'{}' object has no attribute '{}'".format(cls, name)
-            raise AttributeError(errmsg)
+    __slots__ = ['first_name']
+    def __init__(self):
+        pass
