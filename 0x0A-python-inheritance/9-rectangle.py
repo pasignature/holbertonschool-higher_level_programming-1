d1f313e78e1c164b7c660a01cb66803479f5da10
Stephen Chu <stephen.chu530@gmail.com>
2018-10-15 11:56:55 -0700

    fix: changed to importing

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,31 +2,14 @@
 """Full Rectangle Module"""
 
 
-class BaseGeometry:
-    """Basic class definition"""
-
-    def area(self):
-        """Empty method only used to raise Exception message"""
-
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Validates the value"""
-
-        if type(value) is not int:
-            raise TypeError(name + ' must be an integer')
-        if value <= 0:
-            raise ValueError(name + ' must be greater than 0')
-
-
-class Rectangle(BaseGeometry):
+class Rectangle(__import__('7-base_geometry').BaseGeometry):
     """Rectangle Class"""
 
     def __init__(self, width, height):
         """inits the class"""
 
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
         self.__width = width
         self.__height = height
 
