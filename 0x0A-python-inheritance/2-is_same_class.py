dfd5b245145b43c8c53bbf4266b8be66bdc053b6
Stephen Chu <stephen.chu530@gmail.com>
2018-10-15 09:14:49 -0700

    feat: changed logic and fixed docstring

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,15 @@
 
 
 def is_same_class(obj, a_class):
-    """returns True if obj is exactly a_class"""
+    """Checks if obj is a type of a_class
+
+    Args:
+        obj - object to be checked
+
+        a_class - 
+
+    Return
+        bool - True if obj is a type of a_class, False otherwise
+    """
 
     return type(obj) is a_class
