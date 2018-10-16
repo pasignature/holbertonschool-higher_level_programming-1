34252778213c3e5c6991445e77e1d5b87f8b5573
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 14:55:13 -0700

    feat: initial commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pyton3
+"""Student To JSON With Filter"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize"""
+
+        self.first_name = first_name
+        self.last_Name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves the dict repr of Student"""
+
+        if attrs:
+            d = {}
+            for k in attrs:
+                if k in self.__dict__.keys():
+                    d[k] = self.__dict__[k]
+            return d
+        return self.__dict__
