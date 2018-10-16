09216b5ffd80e01c18b342a37f4edb6b1b329dcb
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 12:08:46 -0700

    fix: changed logic to handle different cases

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,6 +6,13 @@ def read_lines(filename="", nb_lines=0):
     """Reads n lines from a text file and prints it to stdout"""
 
     with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            nb_lines and print(line, end='')
-            nb_lines -= 1
+        if nb_lines <= 0:
+            for line in f:
+                print(line, end='')
+        else:
+            for line in f:
+                if nb_lines:
+                    print(line, end='')
+                else:
+                    break
+                nb_lines -= 1
