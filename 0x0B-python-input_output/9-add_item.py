569a0b9edfdada5f469e91e767611aa053b0e6fd
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 13:44:43 -0700

    style: swapped lines for pep8

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import sys
 savefile = __import__('7-save_to_json_file').save_to_json_file
 loadfile = __import__('8-load_from_json_file').load_from_json_file
-import sys
+
 
 """Load, Add, Save"""
 
