96321c2f52a1c292be464af49b360edf786c9d98
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 16:05:49 -0700

    feat: initial commit

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Pascal's Triangle Module"""
+
+
+def pascal_triangle(n):
+    """Returns a list of list of integers repr Pascal's Triangle of n"""
+
+    ans = []
+    if n <= 0:
+        return ans
+    ans.append([1])
+    if n == 1:
+        return ans
+    for i in range(1, n):
+        new = []
+        for i in range(1, len(ans[-1])):
+            new.append(ans[-1][i - 1] + ans[-1][i])
+        ans.append([1] + new + [1])
+    return ans
