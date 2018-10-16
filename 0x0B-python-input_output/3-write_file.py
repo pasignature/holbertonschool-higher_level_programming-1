8ca86f21c199c0dac8ab7b9c037df939712a3701
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 16:53:53 -0700

    fix: added conv of input to string

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns num chars written"""
 
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+        return f.write(str(text))
