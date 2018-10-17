e3efc70947b5a0cad5364308c7f65ee187baa74f
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 21:00:21 -0700

    refactor: shortened even further

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -6,10 +6,9 @@ def pascal_triangle(n):
     """Returns a list of list of integers repr Pascal's Triangle of n"""
 
     ans = []
-    if n <= 0:
-        return ans
-    ans.append([1])
-    for r in range(1, n):
-        prv = ans[-1]
-        ans.append([1] + [prv[i - 1] + prv[i] for i in range(1, r)] + [1])
+    if n > 0:
+        ans.append([1])
+        for r in range(1, n):
+            prv = ans[-1]
+            ans.append([1] + [prv[i - 1] + prv[i] for i in range(1, r)] + [1])
     return ans
