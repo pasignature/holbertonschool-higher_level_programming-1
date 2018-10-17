c98603b426259e306ccfb571d1c3dad1665a026b
Stephen Chu <stephen.chu530@gmail.com>
2018-10-16 18:40:01 -0700

    fix: swapped to using for loop and print line by line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,5 @@ def read_file(filename=""):
     """Reads a text file and prints to stdout"""
 
     with open(filename, "r", encoding='utf-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
