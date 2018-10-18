0b67aa1975135d6f85e42584d8efadd43e989fc2
Stephen Chu <stephen.chu530@gmail.com>
2018-10-18 11:13:29 -0700

    feat: implement size getter/setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,19 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Size Getter"""
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Size Setter"""
+
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """STR output"""
 
