8391165be174cd46528d8dccdd11399b555c6b21
Stephen Chu <stephen.chu530@gmail.com>
2018-10-18 19:55:54 -0700

    feat: implemented create method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,11 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+
+        dummy = cls(1, 1, 1, 1)
+        dummy.update(**dictionary)
+        return dummy
