844b3d2f902d0a905b9268bd0bd70c293fdcd539
Stephen Chu <stephen.chu530@gmail.com>
2018-10-20 13:32:07 -0700

    fix: added check for empty list of dicts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Returns the JSON string repr"""
 
-        if list_dictionaries is None:
+        if list_dictionaries is None or not len(list_dictionaries):
             return "[]"
         return json.dumps(list_dictionaries)
 
