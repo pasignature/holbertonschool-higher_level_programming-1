e0338044dfbe9e4063a4b08d0c0d53afa999b7e8
Stephen Chu <stephen.chu530@gmail.com>
2018-10-22 11:10:21 -0700

    fix: changed id str output

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize Me!"""
 
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
@@ -96,7 +96,7 @@ class Rectangle(Base):
     def __str__(self):
         """STR Output"""
 
-        return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
+        return '[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}'.format(
             self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
