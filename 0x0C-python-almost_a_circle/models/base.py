c7127aee21483b256aaa36b1d576e1affa8ae3af
Stephen Chu <stephen.chu530@gmail.com>
2018-10-22 13:32:59 -0700

    fix: changed logic to handle create for rect or square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,7 +54,10 @@ class Base:
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set"""
 
-        dummy = cls(1, 1)
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        if cls.__name__ == 'Square':
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
