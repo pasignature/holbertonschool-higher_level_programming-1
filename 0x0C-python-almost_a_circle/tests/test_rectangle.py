1ee2c420417ad68969b715fcc00e7465caa1208c
Stephen Chu <stephen.chu530@gmail.com>
2018-10-22 19:32:56 -0700

    fix: added more tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -3,6 +3,8 @@
 
 import unittest
 import pep8
+import sys
+import io
 from models import rectangle
 from models.rectangle import Rectangle as r
 from models.base import Base as b
@@ -141,6 +143,145 @@ class TestBase(unittest.TestCase):
             r(1, 1, 1, False)
 
         self.assertEqual(b._Base__nb_objects, 1)
+
+    def test_area(self):
+        """Test Area"""
+
+        r1 = r(1, 1, 1, 1)
+        r2 = r(4, 6, 2, 3)
+        r3 = r(56, 83, 5, 7)
+
+        self.assertEqual(r1.area(), 1)
+        self.assertEqual(r2.area(), 24)
+        self.assertEqual(r3.area(), 4648)
+
+    def test_display(self):
+        """Test Display"""
+
+        r1 = r(1, 1)
+        r2 = r(1, 2)
+        r3 = r(2, 1)
+        r4 = r(2, 2, 1)
+        r5 = r(2, 2, 0, 1)
+        r6 = r(2, 2, 2, 2)
+        r7 = r(3, 3, 3, 3)
+
+        god_list = ['#\n', '#\n#\n', '##\n', ' ##\n ##\n',
+                    '\n##\n##\n', '\n\n  ##\n  ##\n',
+                    '\n\n\n   ###\n   ###\n   ###\n']
+        obj_list = [r1, r2, r3, r4, r5, r6, r7]
+        out_list = []
+
+        for obj in obj_list:
+            capture = io.StringIO()
+            sys.stdout = capture
+            obj.display()
+            sys.stdout = sys.__stdout__
+            out_list.append(capture.getvalue())
+
+        self.assertListEqual(god_list, out_list)
+
+    def test_str(self):
+        """Test Str"""
+
+        r1 = r(1, 1)
+        r2 = r(1, 2, 5)
+        r3 = r(2, 1)
+        r4 = r(2, 2, 1, id="HAHAHAHA")
+        r5 = r(2, 2, 0, 1)
+        r6 = r(2, 2, 2, 2)
+        r7 = r(3, 3, 3, 3, "MUHAHAHA")
+
+        god_list = ['[Rectangle] (1) 0/0 - 1/1\n',
+                    '[Rectangle] (2) 5/0 - 1/2\n',
+                    '[Rectangle] (3) 0/0 - 2/1\n',
+                    '[Rectangle] (HAHAHAHA) 1/0 - 2/2\n',
+                    '[Rectangle] (4) 0/1 - 2/2\n',
+                    '[Rectangle] (5) 2/2 - 2/2\n',
+                    '[Rectangle] (MUHAHAHA) 3/3 - 3/3\n']
+        obj_list = [r1, r2, r3, r4, r5, r6, r7]
+        out_list = []
+
+        for obj in obj_list:
+            capture = io.StringIO()
+            sys.stdout = capture
+            print(obj)
+            sys.stdout = sys.__stdout__
+            out_list.append(capture.getvalue())
+
+        self.assertListEqual(god_list, out_list)
+
+    def test_update(self):
+        """Test Update"""
+
+        r1 = r(1, 1)
+        r2 = r(1, 2, 5)
+        r3 = r(2, 1)
+        r4 = r(2, 2, 1, id="HAHAHAHA")
+        r5 = r(2, 2, 0, 1)
+        r6 = r(2, 2, 2, 2)
+        r7 = r(3, 3, 3, 3, "MUHAHAHA")
+
+        r1.update(34, 64, 34, 12, 64)
+        with self.assertRaises(ValueError):
+            r2.update(**{'id': 234, 'y': -5})
+        r3.update(None, 20, 590)
+        r4.update(**{'id': 234, 'height': 64})
+        with self.assertRaises(TypeError):
+            r5.update(98, {'width': 236, 'y': 98})
+        with self.assertRaises(TypeError):
+            r6.update("JJJJ", 'L')
+        r7.update()
+
+        god_list = ['[Rectangle] (34) 12/64 - 64/34\n',
+                    '[Rectangle] (234) 5/0 - 1/2\n',
+                    '[Rectangle] (6) 0/0 - 20/590\n',
+                    '[Rectangle] (234) 1/0 - 2/64\n',
+                    '[Rectangle] (98) 0/1 - 2/2\n',
+                    '[Rectangle] (JJJJ) 2/2 - 2/2\n',
+                    '[Rectangle] (MUHAHAHA) 3/3 - 3/3\n']
+        obj_list = [r1, r2, r3, r4, r5, r6, r7]
+        out_list = []
+
+        for obj in obj_list:
+            capture = io.StringIO()
+            sys.stdout = capture
+            print(obj)
+            sys.stdout = sys.__stdout__
+            out_list.append(capture.getvalue())
+
+        self.assertListEqual(god_list, out_list)
+
+    def test_to_dictionary(self):
+        """Test To Dictionary"""
+
+        r1 = r(1, 1)
+        r2 = r(1, 2, 5)
+        r3 = r(2, 1)
+        r4 = r(2, 2, 1, id="HAHAHAHA")
+        r5 = r(2, 2, 0, 1)
+        r6 = r(2, 2, 2, 2)
+        r7 = r(3, 3, 3, 3, "MUHAHAHA")
+
+        d1 = {'id': 1, 'width': 1, 'height': 1, 'x': 0, 'y': 0}
+        d2 = {'id': 2, 'width': 1, 'height': 2, 'x': 5, 'y': 0}
+        d3 = {'id': 3, 'width': 2, 'height': 1, 'x': 0, 'y': 0}
+        d4 = {'id': 'HAHAHAHA', 'width': 2, 'height': 2, 'x': 1, 'y': 0}
+        d5 = {'id': 4, 'width': 2, 'height': 2, 'x': 0, 'y': 1}
+        d6 = {'id': 5, 'width': 2, 'height': 2, 'x': 2, 'y': 2}
+        d7 = {'id': 'MUHAHAHA', 'width': 3, 'height': 3, 'x': 3, 'y': 3}
+
+        self.assertDictEqual(r1.to_dictionary(), d1)
+        self.assertDictEqual(r2.to_dictionary(), d2)
+        self.assertDictEqual(r3.to_dictionary(), d3)
+        self.assertDictEqual(r4.to_dictionary(), d4)
+        self.assertDictEqual(r5.to_dictionary(), d5)
+        self.assertDictEqual(r6.to_dictionary(), d6)
+        self.assertDictEqual(r7.to_dictionary(), d7)
+
+        r6.update(**{'id': "WUT", "x": 999, "height": 888})
+        d6 = {'id': 'WUT', 'width': 2, 'height': 888, 'x': 999, 'y': 2}
+        self.assertDictEqual(r6.to_dictionary(), d6)
 
 if __name__ == '__main__':
     unittest.main()
