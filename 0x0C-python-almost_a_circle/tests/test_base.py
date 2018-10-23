7c35714516ee0b8be42733d308f0ed5a43e520d6
Stephen Chu <stephen.chu530@gmail.com>
2018-10-23 10:02:57 -0700

    fix: expanded out short names to long names

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -5,9 +5,9 @@ import unittest
 import pep8
 from  os import path, remove
 from models import base
-from models.base import Base as b
-from models.rectangle import Rectangle as r
-from models.square import Square as s
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
     """Class Test Base"""
@@ -15,8 +15,8 @@ class TestBase(unittest.TestCase):
     def tearDown(self):
         """Tear Down"""
 
-        b._Base__nb_objects = 0
-        self.assertEqual(b._Base__nb_objects, 0)
+        Base._Base__nb_objects = 0
+        self.assertEqual(Base._Base__nb_objects, 0)
 
     def test_pep8(self):
         """Test Pep8"""
@@ -28,7 +28,7 @@ class TestBase(unittest.TestCase):
     def test_attr_method_presence(self):
         """Test Attribute Method Presence"""
 
-        b_list = dir(b)
+        b_list = dir(Base)
         self.assertIn("_Base__nb_objects", b_list)
         self.assertIn("__init__", b_list)
         self.assertIn("to_json_string", b_list)
@@ -40,61 +40,61 @@ class TestBase(unittest.TestCase):
     def test_docstring(self):
         """Test Docstring"""
 
-        b1_list = dir(b())
+        b1_list = dir(Base())
         self.assertIn("id", b1_list)
 
         self.assertIsNot(base.__doc__, None)
-        self.assertIsNot(b.__doc__, None)
-        self.assertIsNot(b.__init__.__doc__, None)
-        self.assertIsNot(b.to_json_string.__doc__, None)
-        self.assertIsNot(b.save_to_file.__doc__, None)
-        self.assertIsNot(b.from_json_string.__doc__, None)
-        self.assertIsNot(b.create.__doc__, None)
-        self.assertIsNot(b.load_from_file.__doc__, None)
+        self.assertIsNot(Base.__doc__, None)
+        self.assertIsNot(Base.__init__.__doc__, None)
+        self.assertIsNot(Base.to_json_string.__doc__, None)
+        self.assertIsNot(Base.save_to_file.__doc__, None)
+        self.assertIsNot(Base.from_json_string.__doc__, None)
+        self.assertIsNot(Base.create.__doc__, None)
+        self.assertIsNot(Base.load_from_file.__doc__, None)
 
     def test_instance(self):
         """Test Instantiation"""
 
-        b1 = b()
-        b2 = b(3)
-        b3 = b(None)
-        b4 = b(2.5)
-        b5 = b("Hi")
-        b6 = b(float('nan'))
-        b7 = b(['a', 1, [4, 'y']])
-        b8 = b(None)
+        b1 = Base()
+        b2 = Base(3)
+        b3 = Base(None)
+        b4 = Base(2.5)
+        b5 = Base("Hi")
+        b6 = Base(float('nan'))
+        b7 = Base(['a', 1, [4, 'y']])
+        b8 = Base(None)
 
-        self.assertIsInstance(b1, b)
+        self.assertIsInstance(b1, Base)
         self.assertEqual(b1.id, 1)
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b2, b)
+        self.assertIsInstance(b2, Base)
         self.assertEqual(b2.id, 3)
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b3, b)
+        self.assertIsInstance(b3, Base)
         self.assertEqual(b3.id, 2)
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b4, b)
+        self.assertIsInstance(b4, Base)
         self.assertEqual(b4.id, 2.5)
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b5, b)
+        self.assertIsInstance(b5, Base)
         self.assertEqual(b5.id, "Hi")
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b6, b)
+        self.assertIsInstance(b6, Base)
         self.assertNotEqual(b6.id, float('nan'))
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b7, b)
+        self.assertIsInstance(b7, Base)
         self.assertEqual(b7.id, ['a', 1, [4, 'y']])
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
-        self.assertIsInstance(b8, b)
+        self.assertIsInstance(b8, Base)
         self.assertEqual(b8.id, 3)
-        self.assertEqual(b._Base__nb_objects, 3)
+        self.assertEqual(Base._Base__nb_objects, 3)
 
     def test_to_json_string(self):
         """Test To Json String"""
@@ -103,20 +103,20 @@ class TestBase(unittest.TestCase):
         d2 = [{'a': 'a'}]
         d3 = [{'a': 1, 'b': 2}]
         d4 = [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}]
-        self.assertEqual(b.to_json_string(d1), '[]')
-        self.assertEqual(b.to_json_string(d2), '[{"a": "a"}]')
-        self.assertEqual(b.to_json_string(d3), '[{"a": 1, "b": 2}]')
-        self.assertEqual(b.to_json_string(d4),
+        self.assertEqual(Base.to_json_string(d1), '[]')
+        self.assertEqual(Base.to_json_string(d2), '[{"a": "a"}]')
+        self.assertEqual(Base.to_json_string(d3), '[{"a": 1, "b": 2}]')
+        self.assertEqual(Base.to_json_string(d4),
                          '[{"a": 1, "b": 2}, {"c": 3, "d": 4}]')
         d1 = None
         d2 = [[1, 2]]
         d3 = "Not a dictionary"
         d4 = 666
-        self.assertEqual(b.to_json_string(d1), '[]')
-        self.assertEqual(b.to_json_string(d2), '[[1, 2]]')
-        self.assertEqual(b.to_json_string(d3), '"Not a dictionary"')
+        self.assertEqual(Base.to_json_string(d1), '[]')
+        self.assertEqual(Base.to_json_string(d2), '[[1, 2]]')
+        self.assertEqual(Base.to_json_string(d3), '"Not a dictionary"')
         with self.assertRaises(TypeError):
-            b.to_json_string(d4)
+            Base.to_json_string(d4)
 
     def test_from_json_string(self):
         """Test From Json String"""
@@ -125,66 +125,66 @@ class TestBase(unittest.TestCase):
         d2 = [{'a': 'a'}]
         d3 = [{'a': 1, 'b': 2}]
         d4 = [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}]
-        s1 = b.to_json_string(d1)
-        s2 = b.to_json_string(d2)
-        s3 = b.to_json_string(d3)
-        s4 = b.to_json_string(d4)
-        self.assertEqual(b.from_json_string(s1), d1)
-        self.assertEqual(b.from_json_string(s2), d2)
-        self.assertEqual(b.from_json_string(s3), d3)
-        self.assertEqual(b.from_json_string(s4), d4)
+        s1 = Base.to_json_string(d1)
+        s2 = Base.to_json_string(d2)
+        s3 = Base.to_json_string(d3)
+        s4 = Base.to_json_string(d4)
+        self.assertEqual(Base.from_json_string(s1), d1)
+        self.assertEqual(Base.from_json_string(s2), d2)
+        self.assertEqual(Base.from_json_string(s3), d3)
+        self.assertEqual(Base.from_json_string(s4), d4)
 
         d1 = None
         d2 = [[1, 2]]
         d3 = "Not a dictionary"
-        s1 = b.to_json_string(d1)
-        s2 = b.to_json_string(d2)
-        s3 = b.to_json_string(d3)
-        self.assertEqual(b.from_json_string(s1), [])
-        self.assertEqual(b.from_json_string(s2), d2)
-        self.assertEqual(b.from_json_string(s3), d3)
+        s1 = Base.to_json_string(d1)
+        s2 = Base.to_json_string(d2)
+        s3 = Base.to_json_string(d3)
+        self.assertEqual(Base.from_json_string(s1), [])
+        self.assertEqual(Base.from_json_string(s2), d2)
+        self.assertEqual(Base.from_json_string(s3), d3)
 
     def test_create(self):
         """Test Create"""
 
         r1d = {'id': 1, 'width': 1, 'height': 2, 'x': 3, 'y': 4}
-        r2 = r.create(**r1d)
+        r2 = Rectangle.create(**r1d)
         self.assertDictEqual(r1d, r2.to_dictionary())
 
         s1d = {'id': 1, 'size': 1, 'x': 2, 'y': 3}
-        s2 = s.create(**s1d)
+        s2 = Square.create(**s1d)
         self.assertDictEqual(s1d, s2.to_dictionary())
 
-        r1 = r(1, 2)
+        r1 = Rectangle(1, 2)
         r1d = r1.to_dictionary()
-        r2 = r.create(**r1d)
+        r2 = Rectangle.create(**r1d)
         self.assertDictEqual(r1d, r2.to_dictionary())
 
-        r1 = r(1, 2, 3, 4)
+        r1 = Rectangle(1, 2, 3, 4)
         r1d = r1.to_dictionary()
-        r2 = r.create(**r1d)
+        r2 = Rectangle.create(**r1d)
         self.assertDictEqual(r1d, r2.to_dictionary())
 
-        s1 = s(1)
+        s1 = Square(1)
         s1d = s1.to_dictionary()
-        s2 = s.create(**s1d)
+        s2 = Square.create(**s1d)
         self.assertDictEqual(s1d, s2.to_dictionary())
 
-        s1 = s(1, 2, 3)
+        s1 = Square(1, 2, 3)
         s1d = s1.to_dictionary()
-        s2 = s.create(**s1d)
+        s2 = Square.create(**s1d)
         self.assertDictEqual(s1d, s2.to_dictionary())
 
     def test_save_to_file(self):
         """Test Save To File"""
 
-        r1 = r(10, 7, 2, 8)
-        r2 = r(2, 4)
-        r.save_to_file([r1, r2])
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
 
-        s1 = s(6, 5, 3)
-        s2 = s(9)
-        s.save_to_file([s1, s2])
+        s1 = Square(6, 5, 3)
+        s2 = Square(9)
+        Square.save_to_file([s1, s2])
 
         self.assertTrue(path.isfile('Rectangle.json'))
         self.assertTrue(path.isfile('Square.json'))
@@ -192,23 +192,23 @@ class TestBase(unittest.TestCase):
     def test_load_from_file(self):
         """Test Load From File"""
 
-        r1 = r(10, 7, 2, 8)
-        r2 = r(2, 4)
-        s1 = s(6, 5, 3)
-        s2 = s(9)
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        s1 = Square(6, 5, 3)
+        s2 = Square(9)
 
-        rlist = r.load_from_file()
-        slist = s.load_from_file()
+        rlist = Rectangle.load_from_file()
+        slist = Square.load_from_file()
 
-        self.assertIsInstance(rlist[0], r)
-        self.assertIsInstance(rlist[1], r)
+        self.assertIsInstance(rlist[0], Rectangle)
+        self.assertIsInstance(rlist[1], Rectangle)
         self.assertDictEqual(rlist[0].to_dictionary(),
                              r1.to_dictionary())
         self.assertDictEqual(rlist[1].to_dictionary(),
                              r2.to_dictionary())
 
-        self.assertIsInstance(slist[0], s)
-        self.assertIsInstance(slist[1], s)
+        self.assertIsInstance(slist[0], Square)
+        self.assertIsInstance(slist[1], Square)
         self.assertDictEqual(slist[0].to_dictionary(),
                              s1.to_dictionary())
         self.assertDictEqual(slist[1].to_dictionary(),
