39155a6b8a2639cb71f29ad308f44d30452aff87
Stephen Chu <stephen.chu530@gmail.com>
2018-12-03 13:13:31 -0800

    fix: capitalize key words

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Displays the top 3 cities temps during July and August ordered by temp desc
-SELECT city, AVG(value) as avg_temp FROM temperatures where month IN (7, 8) GROUP BY CITY ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY CITY ORDER BY avg_temp DESC LIMIT 3;
