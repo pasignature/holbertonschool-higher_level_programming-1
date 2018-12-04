37b33d04da6796386ca66487d0800e157e69dc6a
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 13:37:03 -0800

    feat: initial commit

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Use the DB and lists all genres of a show
+SELECT a.name
+FROM tv_genres a
+JOIN tv_show_genres b
+ON a.id = b.genre_id
+JOIN tv_shows c
+ON c.id = b.show_id
+WHERE c.title = 'Dexter'
+ORDER BY a.name ASC;
