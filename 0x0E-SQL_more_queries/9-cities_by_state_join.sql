d0139f50bd351f75a473117ab9ddcf9e02dd46d6
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 13:28:12 -0800

    fix: made minor formatting changes

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- List all cities in a database
 SELECT cities.id, cities.name, states.name
 FROM cities
-     INNER JOIN
-     	   states ON cities.state_id = states.id
+JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
