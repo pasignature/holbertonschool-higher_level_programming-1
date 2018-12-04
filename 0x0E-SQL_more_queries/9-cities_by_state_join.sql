862be9a75ed22f3a4e54243b661f84e2285d966b
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 11:19:18 -0800

    fix: used JOIN keyword

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- List all cities in a database
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+     INNER JOIN
+     	   states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
