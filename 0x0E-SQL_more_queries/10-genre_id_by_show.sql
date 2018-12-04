8ba887ccbd37eab42994525d37975ddf730885f7
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 12:43:00 -0800

    fix: added where checks

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all tv shows
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-GROUP BY tv_shows.title, tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT a.title, b.genre_id
+FROM tv_shows a, tv_show_genres b
+where b.show_id = a.id
+ORDER BY a.title ASC, b.genre_id ASC;
