62140c34aa660486ba5961bf0e6110e887f5d35f
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 13:57:32 -0800

    feat: initial commit

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List all shows and all genres linked to that show from the DB
+SELECT a.title, c.name
+FROM tv_shows a
+LEFT JOIN tv_show_genres b
+ON a.id = b.show_id
+LEFT JOIN tv_genres c
+ON b.genre_id = c.id
+ORDER BY a.title ASC;
