d412af4ece7fef06cc80d7e418b4d76705ccc1e5
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 12:04:57 -0800

    fix: separated out primary key keyword

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- Creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256),
+       PRIMARY KEY (id),
        FOREIGN KEY (state_id) REFERENCES states(id)
 );
