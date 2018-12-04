e33dd2de0e02b50e249ed1ab1dff5120c8094f8e
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 12:33:34 -0800

    fix: added where clause and AND keyword

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- Lists all Cali cities that is in the database
 SELECT cities.id, cities.name
-FROM states, cities
-WHERE states.name = 'California'
+FROM cities, states
+WHERE cities.state_id = states.id AND
+      states.name = 'California'
 ORDER BY cities.id ASC;
