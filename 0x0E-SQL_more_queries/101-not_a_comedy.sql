9d1698dfa188faeb88ee4ea9e482d3f9aa12e7d7
Stephen Chu <stephen.chu530@gmail.com>
2018-12-04 19:07:56 -0800

    fix: changed logic

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,12 @@
--- List all show wtihout the comedy genre
+-- List all shows not from the comedy genre
 SELECT d.title
-FROM (SELECT a.title, c.name
+FROM tv_shows d
+WHERE d.title NOT IN
+     (SELECT a.title
      FROM tv_shows a
-     LEFT JOIN tv_show_genres b
+     JOIN tv_show_genres b
      ON a.id = b.show_id
-     LEFT JOIN tv_genres c
-     ON b.genre_id = c.id) d
-WHERE d.name != 'Comedy' OR d.name IS NULL
-GROUP BY d.title
+     JOIN tv_genres c
+     ON b.genre_id = c.id
+     WHERE c.name = 'Comedy')
 ORDER BY d.title ASC;
