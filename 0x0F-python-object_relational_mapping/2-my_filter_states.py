6ed59ded6cb712c77dda9bfc15f3dbda521c5fc0
Stephen Chu <stephen.chu530@gmail.com>
2019-01-02 09:27:45 -0800

    fix: protect against execution when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,17 +4,18 @@ A script that lists all states where name matches argument from
 the database hbtn_0e_0_usa
 '''
 
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
-db.close()
-rows = cur.fetchall()
-for row in rows:
-    print("{}".format(row))
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
+    db.close()
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row))
