72012059a3cfa52b212191138adf5f11c844e70b
Stephen Chu <stephen.chu530@gmail.com>
2019-01-02 10:29:58 -0800

    fix: order by states.id

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
     db.close()
     rows = cur.fetchall()
     for row in rows:
