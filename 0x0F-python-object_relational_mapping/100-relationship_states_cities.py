3d0c9ef1cd0443d20bed7cfd31998d8c48871af0
Stephen Chu <stephen.chu530@gmail.com>
2019-01-03 11:27:25 -0800

    fix: changed add new record logic

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
-    session.add(State(name='California'))
-    session.commit()
-    id_num = [row.id for row in session.query(State).
-              filter(State.name == 'California')][0]
-    session.add(City(name='San Francicso', state_id=id_num))
+    s = State(name='California')
+    c = City(name='San Francicso')
+    s.cities.append(c)
+    session.add(s)
+    session.add(c)
     session.commit()
     session.close()
