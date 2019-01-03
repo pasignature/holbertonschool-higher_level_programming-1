f4b33cef3067a4178c8b5614ed57d286c551ad41
Stephen Chu <stephen.chu530@gmail.com>
2019-01-02 18:01:46 -0800

    fix: changed to using session

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,7 +4,8 @@
 import sys
 from model_state import Base, State
 
-from sqlalchemy import create_engine, select, MetaData, Table
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
@@ -12,9 +13,10 @@ if __name__ == "__main__":
                                   sys.argv[2],
                                   sys.argv[3]),
                            pool_pre_ping=True)
-    states = Table('states', MetaData(bind=None), autoload_with=engine)
-    result = engine.connect().execute(select([states])).fetchone()
-    if result and result[0] and result[1]:
-        print("{}: {}".format(result[0], result[1]))
+    session = sessionmaker(bind=engine)()
+    result = session.query(State).first()
+    if result:
+        print(result)
     else:
         print("Nothing")
+    session.close()
