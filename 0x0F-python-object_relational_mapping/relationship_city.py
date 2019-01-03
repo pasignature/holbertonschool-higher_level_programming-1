17f857be0c570086fd7dd7e356896af4787a70f5
Stephen Chu <stephen.chu530@gmail.com>
2019-01-03 10:56:51 -0800

    feat: initial commit

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,7 +5,7 @@ Contains the class def of City
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,6 +5,7 @@ Base = declarative_base()
 '''
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
