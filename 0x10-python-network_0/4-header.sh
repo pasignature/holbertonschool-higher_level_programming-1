38d448e76b23a8eecfdb984b56a6f7af1149bc6d
Stephen Chu <stephen.chu530@gmail.com>
2019-01-15 12:37:04 -0800

    fix: shortened command

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# SCript that sends a GET request and displays response
-curl -sX GET -H "X-HolbertonSchool-User-Id:98" "$1"
+# Script that sends header var
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
