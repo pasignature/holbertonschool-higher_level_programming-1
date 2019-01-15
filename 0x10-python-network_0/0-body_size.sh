1e93c0612784e1cbbdb9c1a3b1f176c802c6f1da
Stephen Chu <stephen.chu530@gmail.com>
2019-01-15 12:08:14 -0800

    refactor: used grep instead

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Takes in a URL, sends a request, and displays the size of the body
-curl -s -o /dev/null -sw "%{size_download}" "$1"; echo ""
+curl -sI "$1" | grep Content-Length | cut -d' ' -f2
