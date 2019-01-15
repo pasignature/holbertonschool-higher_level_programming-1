800a8efa352f9587afa08a41708952086ec6e7ce
Stephen Chu <stephen.chu530@gmail.com>
2019-01-15 12:05:43 -0800

    fix: added s flag

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Takes in a URL, sends a request, and displays the size of the body
-curl -o /dev/null -sw "%{size_download}" "$1"; echo ""
+curl -s -o /dev/null -sw "%{size_download}" "$1"; echo ""
