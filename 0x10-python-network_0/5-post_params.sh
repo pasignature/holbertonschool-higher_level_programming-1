070665928ddf9ee57ad1250eba647d76101c9c3b
Stephen Chu <stephen.chu530@gmail.com>
2019-01-15 12:25:52 -0800

    fix: add s flag and changed shebang

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Sends a POST request and displays response
-curl -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
