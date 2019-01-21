71106fefc675bc965f2cb0e38e3732e82413df46
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 12:15:04 -0800

    fix: changed tab to 4 spaces

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ from requests import get
 if __name__ == "__main__":
     with get("https://intranet.hbtn.io/status") as r:
         print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.content.decode('utf-8')))
+        print("    - type: {}".format(type(r.text)))
+        print("    - content: {}".format(r.content.decode('utf-8')))
