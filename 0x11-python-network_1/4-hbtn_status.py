5b1296264cea2bdd7eb25cf75794bdb86a7d76e4
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 09:31:18 -0800

    feat: initial commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Fetches from https://intranet.hbtn.io/status'''
+from requests import get
+
+
+if __name__ == "__main__":
+    with get("https://intranet.hbtn.io/status") as r:
+        print("Body response:")
+        print("\t- type: {}".format(type(r.text)))
+        print("\t- content: {}".format(r.content))
