67df14a52faa70203e7a860f34853affa153977e
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 12:11:31 -0800

    fix: added decode method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     with get("https://intranet.hbtn.io/status") as r:
         print("Body response:")
         print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.content))
+        print("\t- content: {}".format(r.content.decode('utf-8')))
