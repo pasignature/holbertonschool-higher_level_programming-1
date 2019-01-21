c842495a806f14b2221b2d7eb927bb4a7a8e2b29
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 15:06:33 -0800

    fix: reversed argv positions

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''List the ten most recent commits for a github repo'''
 from sys import argv
-from requests import Session, Request, get
+from requests import get
 
 
 if __name__ == "__main__":
-    u = 'https://api.github.com/repos/' + argv[1] + '/' + argv[2] + '/commits'
+    u = 'https://api.github.com/repos/' + argv[2] + '/' + argv[1] + '/commits'
     r = get(u)
     try:
         out = r.json()
