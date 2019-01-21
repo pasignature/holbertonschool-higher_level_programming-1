4a04ecbec6cc125207ac337c5c7a9276737b6c10
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 10:34:15 -0800

    feat: initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Takes in a letter and send a POST request'''
+from requests import get
+from requests.exceptions import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    q = ""
+    if len(argv) > 1:
+        q = argv[1]
+    r = post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        out = r.json()
+    except:
+        print("Not a valid JSON")
+    else:
+        if out:
+            print("[{}] {}".format(out['id'], out['name']))
+        else:
+            print("No result")
