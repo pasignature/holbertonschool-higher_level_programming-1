de9ff07b3634d19bbf740309fd61d950c8fc35f1
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 18:55:43 -0800

    fix: made helper function to print entries

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 '''Takes in a string and sends a search request to SWAPI'''
-from requests import post, get
+from requests import get
 from sys import argv
 
 
-def print_titles(films):
-    '''display titles wth tabulation'''
-    for film in films:
-        print("\t{}".format(get(film).json().get('title')))
+def print_entry(r):
+    '''Prints character and title'''
+    for entry in r.get('results'):
+        print(entry.get('name'))
+        for film in entry.get('films'):
+            print("\t{}".format(get(film).json().get('title')))
 
 
 if __name__ == "__main__":
-    r = post('https://swapi.co/api/people', params={'search': argv[1]}).json()
+    r = get('https://swapi.co/api/people', params={'search': argv[1]}).json()
     print("Number of results: {}".format(r.get('count')))
-    while (1):
-        for entry in r.get('results'):
-            print(entry.get('name'))
-            print_titles(entry.get('films'))
-        if r.get('next'):
-            r = get(r.get('next')).json()
-        else:
-            break
+    print_entry(r)
+    while r.get('next'):
+        r = get(r.get('next')).json()
+        print_entry(r)
