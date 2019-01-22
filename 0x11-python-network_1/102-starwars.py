f3f3f4713baac4efcb41e2a25cc6d4fbd1a2d142
Stephen Chu <stephen.chu530@gmail.com>
2019-01-21 17:53:57 -0800

    feat: initial commit

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Takes in a string and sends a search request to SWAPI'''
+from requests import post, get
+from sys import argv
+
+
+def print_titles(films):
+    '''display titles wth tabulation'''
+    for film in films:
+        print("\t{}".format(get(film).json()['title']))
+
+
+if __name__ == "__main__":
+    r = post('https://swapi.co/api/people', params={'search': argv[1]}).json()
+    print("Number of results: {}".format(r['count']))
+    while (1):
+        for entry in r['results']:
+            print(entry['name'])
+            print_titles(entry['films'])
+        if r['next']:
+            r = get(r['next']).json()
+        else:
+            break
