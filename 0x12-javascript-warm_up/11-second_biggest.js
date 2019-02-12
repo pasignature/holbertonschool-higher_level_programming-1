3d64dfd1c6459877828d566a3c45e5fcb680a93d
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 13:11:11 -0800

    fix: changed logic again

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,17 +5,6 @@ let size = process.argv.length;
 if (size <= 3) {
   console.log('0');
 } else {
-  let first = process.argv[2];
-  let sec = process.argv[2] - 1;
-  for (let i = 2; i < size; i++) {
-    if (process.argv[i] > first) {
-      sec = first;
-      first = process.argv[i];
-    } else {
-      if (process.argv[i] > sec) {
-	sec = process.argv[i];
-      }
-    }
-  }
-  console.log(sec);
+  let args = process.argv.sort().reverse();
+  console.log(args[1]);
 }
