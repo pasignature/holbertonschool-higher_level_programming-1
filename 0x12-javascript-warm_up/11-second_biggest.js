3b621731d9d1da432985ade058d40a8434c779b6
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 13:57:30 -0800

    fix: trying set

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,5 +4,10 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  console.log(process.argv.sort()[process.argv.length - 2]);
+  let arr = [...new Set(process.argv.slice(2))].sort().reverse();
+  if (arr.length === 1) {
+    console.log(arr[0]);
+  } else {
+    console.log(arr[1]);
+  }
 }
