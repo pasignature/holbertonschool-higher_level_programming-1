b04e0579aa5fc4c62cc0aff745bc87d038a5ca07
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 13:34:44 -0800

    fix: removed equality check

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Searches for second biggest number
 
-if (process.argv.length <= 3) {
+if (process.argv.length < 3) {
   console.log('0');
 } else {
   let arr = process.argv.splice(2).sort();
