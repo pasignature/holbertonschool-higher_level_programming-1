9ed849b2b713ab11c2bca6fceee8eadd963fd743
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 13:49:27 -0800

    fix: changed str 0 to int 0 output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 // Searches for second biggest number
 
 if (process.argv.length <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   console.log(process.argv.sort()[process.argv.length - 2]);
 }
