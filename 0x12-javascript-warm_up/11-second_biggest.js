429befc3700595937ff93fe9a9d8ed0a6e0d1499
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 13:48:01 -0800

    fix: this is getting bad

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Searches for second biggest number
 
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  console.log(process.argv.sort().reverse()[1]);
+  console.log(process.argv.sort()[process.argv.length - 2]);
 }
