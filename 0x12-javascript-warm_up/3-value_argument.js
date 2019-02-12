49793253d719168a72525b696bbf707cf33583f5
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 12:45:26 -0800

    fix: changed logic to not use length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // Prints 3 lines to the screen
 
-let size = parseInt(process.argv.length);
-if (size === 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
