44983e22d1a9992f36a14abcad56d3ffb6418ebd
Stephen Chu <stephen.chu530@gmail.com>
2019-02-12 18:25:06 -0800

    feat: initial commit

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Adv problem that does stuff
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value += 1;
+}
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
