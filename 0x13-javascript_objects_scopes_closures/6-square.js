1070ae81affb1a1a7967f025018924d10434ea5e
Stephen Chu <stephen.chu530@gmail.com>
2019-02-18 12:18:50 -0800

    fix: added undefined check

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,10 @@ module.exports = class Square extends require('./4-rectangle') {
   constructor (size) {
     super(size, size);
   }
-  charPrint (c = 'X') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let y = 0; y < this.height; y++) {
       console.log(c.repeat(this.width));
     }
