2d7bf1e71053ed819259ddfdaeff3ba0f9408c75
Stephen Chu <stephen.chu530@gmail.com>
2019-02-18 12:30:05 -0800

    fix: changed logic

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // imports a dict of occ by user id and computes dict of user id by occ
 let data = require('./101-data').dict;
-let dict = {};
 for (let [k, v] of Object.entries(data)) {
-  if (dict[v] === undefined) {
-    dict[v] = [];
+  if (data[v] === undefined) {
+    data[v] = [];
   }
-  dict[v].push(k);
+  data[v].push(k);
+  delete data[k];
 }
-console.log(dict);
+console.log(data);
