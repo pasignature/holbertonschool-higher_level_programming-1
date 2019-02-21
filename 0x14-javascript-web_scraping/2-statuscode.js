5358816d0fa7f7d1a59d979ea05e7749db4ec447
Stephen Chu <stephen.chu530@gmail.com>
2019-02-20 19:06:59 -0800

    style: added missing space for semistandard compliance

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // displays status code of a get request
-require('request').get(process.argv[2], function(err, res) {
+require('request').get(process.argv[2], function (err, res) {
   if (err) {
     console.log(err);
   } else {
